52fbf0bbddaad1bf6b28f0cd713a7a94bed2f9f4
siddhant41 <145744208+siddhant41@users.noreply.github.com>
2023-09-25 11:50:32 +0530

    Add files via upload

---
+++ Question8/Question8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("ENTER STRING:");
+string x=Console.ReadLine();
+Console.WriteLine("ENTER  SECOND STRING:");
+string y = Console.ReadLine();
+char[] x1 = x.ToCharArray();
+char[] y1 = y.ToCharArray();
+Array.Sort(x1);
+Array.Sort(y1);
+string x2 = new string(x1);
+string y2 = new string(y1);
+if (x2.Equals(y2))
+{
+    Console.WriteLine("TRUE");
+}
+else
+{
+    Console.WriteLine("FALSE");
+}
---
+++ Question9/Question9/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("ENTER STRING:");
+string x=Console.ReadLine();
+string[] arr = x.Split(new char[] { ' ' });
+int index=0;
+int maxlen = 0;
+for(int i=0;i<arr.Length; i++)
+{
+    if (arr[i].Length > maxlen)
+    {
+        index = i;
+    }
+}
+Console.WriteLine(arr[index]);
